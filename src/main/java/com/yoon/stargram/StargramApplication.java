b079800af7276e98a488222c6db6e8160908367f
yoonwongoo <yunyun26@naver.com>
2021-09-12 14:50:17 +0900

    Etc:프로젝트 환경설정 및 UI디자인
    프로젝트시작을 위한 프로젝트 환경설정 및 UI디자인 설계

---
+++ src/main/java/com/yoon/stargram/StargramApplication.java
@@ -0,0 +1,13 @@
+package com.yoon.stargram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StargramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StargramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yoon/stargram/StargramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoon.stargram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StargramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
